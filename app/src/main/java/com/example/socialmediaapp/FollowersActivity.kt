f5a7cc8a6a0a16eff7aaf419bd49def281f15f96
ahmedzanaty19 <ahmedzanaty275@gmail.com>
2025-09-07 18:35:54 +0300

    New Page

--- app/src/main/java/com/example/socialmediaapp/FollowersActivity.kt
+++ app/src/main/java/com/example/socialmediaapp/FollowersActivity.kt
@@ -5,21 +5,24 @@ import android.content.Intent
 import android.graphics.Color
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class FollowersActivity : AppCompatActivity() {
-    @SuppressLint("WrongViewCast")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_followers)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val btn4 : ImageButton = findViewById( R.id.btnBack)
+
+        val intent = Intent (this, ChatsActivity::class.java)
+        btn4.setOnClickListener {
+
+            startActivity(intent)
         }
 
 
